0f966bb1366550db24eef857d24ce00b2e1c8ffb
KinderRiven <1339764596@qq.com>
2021-06-15 18:23:54 +0800

    simple push

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
+GLIB_PATH := /home/hanshukai/googletest/build/lib
+
 all: demo1
 
 demo1:
-	g++ gtest_demo1.cc -o gtest_demo1 -I./include
+	g++ gtest_demo1.cc -o gtest_demo1 -I./include -L$(GLIB_PATH) -lgtest
 
 clean:
 	rm -rf gtest_demo1
--- gtest_demo1.cc
+++ gtest_demo1.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-15 16:13:50
- * @LastEditTime: 2021-06-15 16:17:15
+ * @LastEditTime: 2021-06-15 16:43:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /gtest_demo/gtest_demo1.cc
