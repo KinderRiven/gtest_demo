af752e933106d17efda5441647736ddd783ed912
KinderRiven <1339764596@qq.com>
2021-06-15 18:38:25 +0800

    simple push

--- gtest_demo1.cc
+++ gtest_demo1.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-15 16:13:50
- * @LastEditTime: 2021-06-15 18:27:02
+ * @LastEditTime: 2021-06-15 18:38:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /gtest_demo/gtest_demo1.cc
@@ -11,9 +11,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int add(int a, int b)
+{
+    return a + b;
+}
+
+TEST(example, add)
+{
+    add(1, 2);
+}
+
 int main(int argc, char** argv)
 {
-    printf("InitGoogleTest!\n");
-    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
     return 0;
 }
