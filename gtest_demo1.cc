3d1e45c558ad81959c08644b3eff2157c5d58756
KinderRiven <1339764596@qq.com>
2021-06-16 14:03:37 +0800

    simple push

--- gtest_demo1.cc
+++ gtest_demo1.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-15 16:13:50
- * @LastEditTime: 2021-06-16 14:02:13
+ * @LastEditTime: 2021-06-16 14:03:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /gtest_demo/gtest_demo1.cc
@@ -16,10 +16,21 @@ int add(int a, int b)
     return a + b;
 }
 
+int sub(int a, int b)
+{
+    return a - b;
+}
+
 TEST(example, add)
 {
     int _r = add(1, 2);
     ASSERT_TRUE(_r == 3);
+}
+
+TEST(example, sub)
+{
+    int _r = sub(1, 2);
+    ASSERT_TRUE(_r == 1);
 }
 
 int main(int argc, char** argv)
